09fc0decac2d93e3f2c3fb66b53fdc57e4b7f827
VariableNi <112205692+VariableNi@users.noreply.github.com>
2025-07-31 01:32:27 +0500

    Add files via upload

---
+++ GloomShade/GloomShade/GloomShade.cpp
@@ -0,0 +1,45 @@
+﻿#define GLEW_STATIC
+#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    if (glewInit() != GLEW_OK)
+   {
+        std::cout << "GLEW_IS_NOT_OKAY";
+   }
+    
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
